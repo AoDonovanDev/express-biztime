711d00f2a5dbaf93ebef01561934d98848115839
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-08-05 19:27:10 -0400

    company routes

--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 /** Database setup for BizTime. */
+const { Client } = require('pg')
+const secret = require('./secret')
 
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = `postgresql://postgres:${secret}@localhost/biztime_test`;
+} else {
+  DB_URI = `postgresql://postgres:${secret}@localhost/biztime`;
+}
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db
